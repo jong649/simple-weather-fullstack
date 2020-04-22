05a833400a7b8d9e071597353fca941c81adaed6
Jon <jong649@gmail.com>
2020-04-21 21:08:53 -0700

    add wind data to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,10 +9,14 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback("Unable to find location", undefined);
     } else {
-      callback(
-        undefined,
-        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`
-      );
+      const forecastString = `
+        ${body.current.weather_descriptions[0]}.
+        It is currently ${body.current.temperature} degrees out.
+        It feels like ${body.current.feelslike} degrees out.
+        The wind speed is ${body.current.wind_speed}mph.
+        The wind direction is ${body.current.wind_dir}.
+        `;
+      callback(undefined, forecastString);
     }
   });
 };
