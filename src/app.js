088a7bb851743d9aa2edf87febf137ee992f33d7
Jon <jong649@gmail.com>
2020-04-21 20:38:26 -0700

    hide api keys again

--- src/app.js
+++ src/app.js
@@ -4,8 +4,6 @@ const hbs = require("hbs");
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-console.log(process.env.WEATHERSTACK_KEY);
-
 const app = express();
 const port = process.env.PORT || 3000;
 
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require("request");
 console.log(process.env.WEATHERSTACK_KEY);
 
 const forecast = (latitude, longitude, callback) => {
-  const url = `http://api.weatherstack.com/current?access_key=1eaf9e02b38ff7c781479b09c9cf35d9&query=${latitude},${longitude}&units=f`;
+  const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHERSTACK_KEY}&query=${latitude},${longitude}&units=f`;
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
